44c60a62208f89d70a2db28b2b4525c114defa03
Stephanie Federman <sfederma@yahoo.com>
2022-05-24 21:24:24 -0700

    FE: Finished Home
    Homepage is done, navbar needs reworking, footer needs reworking, forms
    need validation, captcha needs backend

--- src/Home2.tsx
+++ src/Home2.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Button,
   ButtonToolbar,
@@ -17,6 +17,26 @@ import { PickerInstance } from "rsuite/esm/Picker";
 import Background_1 from "./img/background1.jpg";
 
 export const Home2 = () => {
+  const [reCaptcha, setReCaptcha] = useState(
+    <ReCAPTCHA
+      sitekey="6Lfs2A4gAAAAALNUUsbQQiXqNIysLq0Ijpq6akRq"
+      onChange={onChange}
+      size="normal"
+      className="g-recaptcha"
+    />
+  );
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      setReCaptcha(
+        <ReCAPTCHA
+          sitekey="6Lfs2A4gAAAAALNUUsbQQiXqNIysLq0Ijpq6akRq"
+          onChange={onChange}
+          size="compact"
+          className="g-recaptcha"
+        />
+      );
+    }
+  });
   const propertyPicker = forwardRef(
     (props, ref?: React.Ref<PickerInstance>) => (
       <SelectPicker
@@ -48,86 +68,134 @@ export const Home2 = () => {
     <Content
       style={{
         width: "100vw",
-        display: "flex",
-        backgroundColor: "cyan",
         color: "white",
         overflow: "hidden",
-        justifyContent: "center",
-        alignItems: "center",
-        overflowY: "hidden",
-        overflowX: "hidden",
-        height: "calc(100vh - 150px)",
+        background: `url(${Background_1}) no-repeat 0 0`,
+        backgroundSize: "cover",
+        minHeight: "100vh",
       }}
     >
-      <img
-        src={Background_1}
-        style={{
-          objectFit: "cover",
-          opacity: "0.8",
-          width: "100%",
-          position: "absolute",
-          height: "100%",
-          top: "0px",
-          left: "0px",
-        }}
-      />
-      <FlexboxGrid
-        style={{ backgroundColor: "black", width: "25%", height: "100%" }}
-      />
-      <FlexboxGrid
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "75%",
-          textAlign: "center",
-        }}
-      >
-        <FlexboxGrid.Item colspan={24} as={Col}>
-          <h2>To Travel is to Live</h2>
-        </FlexboxGrid.Item>
-        <FlexboxGrid.Item colspan={24} as={Col}>
-          <p>
-            Vacation Homes in the Riviera Maya and Puerto Penasco offered by
-            Cece
-          </p>
-        </FlexboxGrid.Item>
+      <FlexboxGrid style={{ width: "100%" }}>
         <FlexboxGrid.Item
-          colspan={24}
+          lg={6}
+          md={6}
+          smHidden
+          xsHidden
           as={Col}
-          style={{ display: "flex", justifyContent: "center" }}
+          style={{
+            backgroundColor: "cyan",
+            opacity: "0.3",
+            minHeight: "100vh",
+          }}
+        />
+        <FlexboxGrid.Item
+          lg={18}
+          md={18}
+          sm={24}
+          xs={24}
+          as={Col}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            minHeight: "100vh",
+            paddingBottom: "1%",
+          }}
         >
-          <FlexboxGrid>
-            <FlexboxGrid.Item colspan={24}>
-              <Form layout="horizontal">
-                <Form.Group controlId="email">
-                  <Form.ControlLabel>Email</Form.ControlLabel>
-                  <Form.Control name="email" type="email" />
-                  <Form.HelpText tooltip>Email is required</Form.HelpText>
-                </Form.Group>
-                <Form.Group>
-                  <Form.ControlLabel>Check-in & Check-Out:</Form.ControlLabel>
-                  <Form.Control
-                    name="dates"
-                    accepter={datePicker}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                  <Form.ControlLabel>Location:</Form.ControlLabel>
-                  <Form.Control
-                    name="properties"
-                    accepter={propertyPicker}
-                  ></Form.Control>
-                </Form.Group>
-                <ReCAPTCHA
-                  sitekey="6Lfs2A4gAAAAALNUUsbQQiXqNIysLq0Ijpq6akRq"
-                  onChange={onChange}
-                />
-                <ButtonToolbar>
-                  <Button appearance="primary" type="submit">
-                    Submit
-                  </Button>
-                </ButtonToolbar>
-              </Form>
+          <FlexboxGrid
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              justifyContent: "center",
+              maxWidth: "95%",
+              alignItems: "center",
+            }}
+          >
+            <FlexboxGrid.Item
+              colspan={24}
+              as={Col}
+              style={{
+                maxWidth: "90%",
+                display: "inline-block",
+                margin: "5% 0 5% 0",
+              }}
+            >
+              <h2 className="title">To Travel is to Live</h2>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item
+              colspan={24}
+              as={Col}
+              style={{
+                maxWidth: "90%",
+                display: "inline-block",
+                margin: "5% 0 5% 0",
+              }}
+            >
+              <p className="subtext">
+                Vacation Homes in the Riviera Maya and Puerto Penasco offered by
+                Cece
+              </p>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item
+              colspan={24}
+              as={Col}
+              style={{ display: "inline-block" }}
+            >
+              <FlexboxGrid>
+                <FlexboxGrid.Item colspan={24}>
+                  <Panel
+                    shaded
+                    bordered
+                    style={{
+                      backgroundColor: "white",
+                      color: "gray",
+                    }}
+                  >
+                    <Form
+                      fluid
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        fontSize: "1.3em",
+                      }}
+                    >
+                      <Form.Group controlId="email">
+                        <Form.ControlLabel>Email</Form.ControlLabel>
+                        <Form.Control name="email" type="email" />
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.ControlLabel>
+                          Check-in & Check-Out:
+                        </Form.ControlLabel>
+                        <Form.Control
+                          name="dates"
+                          accepter={datePicker}
+                        ></Form.Control>
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.ControlLabel>Location:</Form.ControlLabel>
+                        <Form.Control
+                          name="properties"
+                          accepter={propertyPicker}
+                        ></Form.Control>
+                      </Form.Group>
+                      <div style={{ textAlign: "center", margin: "2% 0 2% 0" }}>
+                        {reCaptcha}
+                      </div>
+                      <ButtonToolbar>
+                        <Button
+                          appearance="primary"
+                          type="submit"
+                          size="lg"
+                          block
+                        >
+                          Submit
+                        </Button>
+                      </ButtonToolbar>
+                    </Form>
+                  </Panel>
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
             </FlexboxGrid.Item>
           </FlexboxGrid>
         </FlexboxGrid.Item>
--- src/Testimonials.tsx
+++ src/Testimonials.tsx
@@ -23,6 +23,8 @@ export const Testimonials = () => {
         textAlign: "center",
         backgroundColor: "cyan",
         color: "white",
+        background: `url(${Background_1}) no-repeat 0 0`,
+        backgroundSize: "cover",
       }}
     >
       <FlexboxGrid
@@ -43,22 +45,12 @@ export const Testimonials = () => {
             justifyContent: "center",
             alignItems: "center",
             padding: "5% 2% 5% 2%",
-            overflowY: "hidden",
+            backgroundColor: "rgba(0, 255, 255, 0.3)",
           }}
         >
           <h1 style={{ fontFamily: "cursive", zIndex: "5" }}>
             We create awesome experiences
           </h1>
-          <img
-            src={Background_1}
-            style={{
-              objectFit: "cover",
-              objectPosition: "middle left",
-              opacity: "0.8",
-              width: "100%",
-              position: "absolute",
-            }}
-          />
         </FlexboxGrid.Item>
         <FlexboxGrid.Item
           as={Col}
@@ -69,102 +61,74 @@ export const Testimonials = () => {
             alignItems: "center",
             alignContent: "center",
             flexDirection: "column",
-            padding: "5% 0 2% 0 ",
-            color: "black",
-            backgroundColor: "white",
+            padding: "2% 0 2% 0 ",
+            color: "gray",
           }}
         >
-          <h2 style={{ fontFamily: "cursive" }}>Testimonials</h2>
-          <Carousel
-            key={`${placement}.${shape}`}
-            placement={placement}
-            shape={shape}
-            className="custom-slider"
-            autoplay
-            style={{
-              margin: "5% 0 5% 0",
-              padding: "5% 0 5% 0",
-            }}
-          >
-            <div>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                style={{ height: "25%" }}
-              />
-              <p>Name</p>
-              <p>Date</p>
-              <p>Description</p>
-            </div>
-            <div>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                style={{ height: "25%" }}
-              />
-              <p>Name</p>
-              <p>Date</p>
-              <p>Description</p>
-            </div>
-            <div>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                style={{ height: "25%" }}
-              />
-              <p>Name</p>
-              <p>Date</p>
-              <p>Description</p>
-            </div>
-            <div>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                style={{ height: "25%" }}
-              />
-              <p>Name</p>
-              <p>Date</p>
-              <p>Description</p>
-            </div>
-            <div>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
-                style={{ height: "25%" }}
-              />
-              <p>Name</p>
-              <p>Date</p>
-              <p>Description</p>
-            </div>
-          </Carousel>
+          <h2 className="subtitle">Testimonials</h2>
+          <Panel shaded className="car-panel">
+            <Carousel
+              key={`${placement}.${shape}`}
+              placement={placement}
+              shape={shape}
+              className="custom-slider"
+              autoplay
+            >
+              <div className="car-slide">
+                <h4>Name</h4>
+                <p>Date</p>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptate beatae possimus id quis delectus incidunt adipisci,
+                  aspernatur corporis odio excepturi vitae error amet
+                  necessitatibus veritatis aperiam omnis, voluptatibus harum
+                  fugit.
+                </p>
+              </div>
+              <div className="car-slide">
+                <p>Name</p>
+                <p>Date</p>
+                <p>Description</p>
+              </div>
+              <div className="car-slide">
+                <p>Name</p>
+                <p>Date</p>
+                <p>Description</p>
+              </div>
+              <div className="car-slide">
+                <p>Name</p>
+                <p>Date</p>
+                <p>Description</p>
+              </div>
+              <div className="car-slide">
+                <p>Name</p>
+                <p>Date</p>
+                <p>Description</p>
+              </div>
+            </Carousel>
+          </Panel>
         </FlexboxGrid.Item>
         <FlexboxGrid.Item
           colspan={24}
           as={Col}
           style={{
             display: "flex",
-            justifyContent: "space-around",
-            padding: "5% 2% 5% 2%",
-            overflowY: "hidden",
+            padding: "3% 0% 3% 0%",
+            backgroundColor: "rgba(0, 255, 255, 0.3)",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          <h2 style={{ fontFamily: "cursive", zIndex: "5" }}>
+          <h2 style={{ fontFamily: "cursive" }}>
             These are just some of our testimonials...
           </h2>
           <Button
             href="www.google.com"
-            style={{ fontFamily: "arial", zIndex: "5", marginTop: "3%" }}
+            style={{ fontFamily: "arial", marginTop: "3%" }}
             size="lg"
           >
             View More
           </Button>
-          <img
-            src={Background_1}
-            style={{
-              objectFit: "cover",
-              objectPosition: "middle left",
-              opacity: "0.8",
-              width: "100%",
-              position: "absolute",
-            }}
-          />
         </FlexboxGrid.Item>
       </FlexboxGrid>
     </Content>
